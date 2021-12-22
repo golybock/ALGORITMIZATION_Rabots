0f376962f1ac24419ea32485eac4e0d3d3b5c9eb
golybock <teplovartem3094@gmail.com>
2021-12-22 10:45:36 +0300

    PR 2

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,21 +6,45 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите длину ряда чисел");
-            int dlina = Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            int biba = 0;
-            while (i < dlina)
+            double x, y, z, biba, boba;
+            Console.WriteLine("Введите x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите z:");
+            z = Convert.ToDouble(Console.ReadLine());
+            if (x < z && x < y)
             {
-                Console.WriteLine($"Введите число");
-                int boba = Convert.ToInt32(Console.ReadLine());
-                i++;
-                if (boba % 2 != 0 && boba < 0)
-                {
-                    biba++;
-                }
+                x = Math.Pow(x, 2);
             }
-            Console.WriteLine($"Отрицательный нечетных чисел:{biba}");
+            else if (y < z && y < x)
+            {
+                y = Math.Pow(y, 2);
+            }
+            else if (z < x && z < y)
+            {
+                z = Math.Pow(z, 2);
+            }
+            else if (x == y && x < z)
+            {
+                x = Math.Pow(x, 2);
+            }
+            else if (x == z && x < y)
+            {
+                x = Math.Pow(x, 2);
+            }
+            else if (y == z && y < x)
+            {
+                x = Math.Pow(y, 2);
+            }
+            else if (y == z && y == x)
+            {
+                x = Math.Pow(x, 2);
+            }
+            biba = (x * ((x + y + z)) / 2) + 1;
+            boba = (x * ((x * y * z)) / 2) + 1;
+            Console.WriteLine(biba);
+            Console.WriteLine(boba);
         }
     }
 }
