3dc2f057c280ff08d0c9264e25f39ac8d0d78789
golybock <teplovartem3094@gmail.com>
2021-12-22 10:43:33 +0300

    PR4

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,41 +6,20 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите n(размер матрицы)");
-            int n = int.Parse(Console.ReadLine()); //размер матрицы
-            int[,] matrix = new int[n, n]; //создание матрицы
+            Console.WriteLine("Введите длину последовательности:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] chisl = new double[n];
+            double sum = 0;
             Random rnd = new Random();
-            //генерация исходной матрицы 
-            Console.WriteLine("Исходная матрица: ");
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                chisl[i] = rnd.Next(0, 10);
+                if (i != 0 && (i%i) == 0)
                 {
-                    matrix[i, j] = rnd.Next(100);
-                    Console.Write(matrix[i, j] + " ");
+                    sum += chisl[i];
                 }
-
-                Console.WriteLine();
             }
-            //обмен значений строки n и столбца n
-            int b = 0;
-            int a = 0;
-            while (b < matrix.GetLength(0) && a < matrix.GetLength(1))
-            {
-                (matrix[n-1, b], matrix[a,n-1]) = (matrix[a, n-1], matrix[n-1,b]);
-                b++;
-                a++;
-            }
-            Console.WriteLine("Матрица с изменениями: ");
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write(matrix[i, j] + " ");
-                }
-
-                Console.WriteLine();
-            }
+            Console.WriteLine(sum);
         }
     }
 }
