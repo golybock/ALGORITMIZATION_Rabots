773927db1ac75cc121470ae9583919aabed07735
golybock <teplovartem3094@gmail.com>
2021-12-22 10:13:05 +0300

    LR 5

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,21 +6,32 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите строку, содержащую хотя бы один пробел.");
-            string s = Console.ReadLine();
-            // индексы пробелов в строке(первого и последнего)
-            int perviiProbel = s.IndexOf(" ");
-            int posledniiProbel = s.LastIndexOf(" ");
-            // длинна новой подстроки
-            int len = posledniiProbel - perviiProbel;
-            string result;
-            // если в строке нет ничего между пробелами
-            if (len <= 1) result = "";
-            // создание подстроки
-            else result = s.Substring(perviiProbel + 1, len - 1);
-            // вывод результата
-            Console.WriteLine(result);
-            
+            Console.WriteLine("Введите размер квадратной матрицы");
+            int m = int.Parse(Console.ReadLine()); //размер матрицы
+            int[,] matrix = new int[m, m];
+            Random rnd = new Random();
+            int sum = 0;
+            Console.WriteLine("Исходная матрица: ");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(100);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i+1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[j, i] % 2 != 0)
+                    {
+                        sum += matrix[j, i];
+                    }
+                }    
+            }
+            Console.WriteLine($"сумма нечетных элементов нижнего треугольника:{sum}");
         }
     }
 }
