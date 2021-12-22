a37691e62c5bd6e21d8080a747abbb218dea5fca
golybock <teplovartem3094@gmail.com>
2021-12-22 14:05:49 +0300

    PR 1 zad 1

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,45 +6,9 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            double x, y, z, biba, boba;
             Console.WriteLine("Введите x:");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите y:");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите z:");
-            z = Convert.ToDouble(Console.ReadLine());
-            if (x < z && x < y)
-            {
-                x = Math.Pow(x, 2);
-            }
-            else if (y < z && y < x)
-            {
-                y = Math.Pow(y, 2);
-            }
-            else if (z < x && z < y)
-            {
-                z = Math.Pow(z, 2);
-            }
-            else if (x == y && x < z)
-            {
-                x = Math.Pow(x, 2);
-            }
-            else if (x == z && x < y)
-            {
-                x = Math.Pow(x, 2);
-            }
-            else if (y == z && y < x)
-            {
-                x = Math.Pow(y, 2);
-            }
-            else if (y == z && y == x)
-            {
-                x = Math.Pow(x, 2);
-            }
-            biba = (x * ((x + y + z)) / 2) + 1;
-            boba = (x * ((x * y * z)) / 2) + 1;
-            Console.WriteLine(biba);
-            Console.WriteLine(boba);
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Результат: {x - 10*Math.Sin(x) + Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5))}");
         }
     }
 }
