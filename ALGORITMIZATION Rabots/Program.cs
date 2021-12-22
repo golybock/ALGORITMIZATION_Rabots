e042f26e3d191b917f00d785c7c2f09550425c87
golybock <teplovartem3094@gmail.com>
2021-12-22 14:10:23 +0300

    PR1 zad 2

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,9 +6,15 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите x:");
-            double x = Double.Parse(Console.ReadLine());
-            Console.WriteLine($"Результат: {x - 10*Math.Sin(x) + Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5))}");
+            Console.WriteLine("Введите скорость лодки:");
+            double v = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите скорость течения реки: ");
+            double v1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Время движения по стоячей воде: ");
+            double t = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Время переджвижения против течения реки:");
+            double t1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Путь пройденный лодкой:{v * t + t1 * (v-v1)}");
         }
     }
 }
