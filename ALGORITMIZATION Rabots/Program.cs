40b921d38994b57d2fdf61fda958152f5d5424a9
golybock <teplovartem3094@gmail.com>
2021-12-22 10:10:12 +0300

    LB 6

---
+++ ALGORITMIZATION Rabots/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ALGORITMIZATION_Rabots
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, содержащую хотя бы один пробел.");
+            string s = Console.ReadLine();
+            // индексы пробелов в строке(первого и последнего)
+            int perviiProbel = s.IndexOf(" ");
+            int posledniiProbel = s.LastIndexOf(" ");
+            // длинна новой подстроки
+            int len = posledniiProbel - perviiProbel;
+            string result;
+            // если в строке нет ничего между пробелами
+            if (len <= 1) result = "";
+            // создание подстроки
+            else result = s.Substring(perviiProbel + 1, len - 1);
+            // вывод результата
+            Console.WriteLine(result);
+        }
+    }
+}
