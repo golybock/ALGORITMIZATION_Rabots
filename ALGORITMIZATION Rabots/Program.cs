60105d098c2ab38da8d8ec7e861372ba62c50604
golybock <teplovartem3094@gmail.com>
2021-12-22 10:21:18 +0300

    empty

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -6,12 +6,7 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            double x, y;
-            Console.WriteLine("Введите x:");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите y:");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(Math.Pow(2, -x) - Math.Sin(x) + Math.Cos(2 * x * y));
+            //пусто мастер
         }
     }
 }
