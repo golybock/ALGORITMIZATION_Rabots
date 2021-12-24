5c9639d2532a1c7949fae9f179b609461589e60b
golybock <teplovartem3094@gmail.com>
2021-12-24 22:30:03 +0300

    Lb 7

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ALGORITMIZATION_Rabots
 {
@@ -6,15 +9,37 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите скорость лодки:");
-            double v = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите скорость течения реки: ");
-            double v1 = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Время движения по стоячей воде: ");
-            double t = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Время переджвижения против течения реки:");
-            double t1 = Double.Parse(Console.ReadLine());
-            Console.WriteLine($"Путь пройденный лодкой:{v * t + t1 * (v-v1)}");
+            Random rnd = new Random();
+            // создание 2 множеств длинной от 2 до 10 элементов
+            int[] X = new int[rnd.Next(1,10)];
+            int[] Y = new int[rnd.Next(1,10)];
+            int count = 0; //счетчик
+            Console.WriteLine("Первое множество:");
+            // создание 1 множества и его вывод
+            for (int i = 0; i < X.Length; i++)
+            {
+                X[i] = rnd.Next(0, 10);
+                Console.Write(X[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Второе множество:");
+            // создание и вывод 2 множества
+            for (int i = 0; i < Y.Length; i++)
+            {
+                Y[i] = rnd.Next(0, 10);
+                Console.Write(Y[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Кол-во общих четных элементов:");
+            // создание множества из общих элементов множеств X и Y
+            IEnumerable<int> Z = X.Intersect(Y);
+            // подсчет четных элементов в этом множестве
+            foreach (var ident in Z)
+            {
+                if (ident % 2 == 0) count++;
+            }
+            // вывод кол-ва четных элементов
+            Console.WriteLine(count);
         }
     }
 }
