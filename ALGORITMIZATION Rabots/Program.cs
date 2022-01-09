36e368ae2d93102a9035a055b6a682c585a824f6
golybock <teplovartem3094@gmail.com>
2022-01-09 11:28:43 +0300

    LR 7

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -10,36 +10,36 @@ namespace ALGORITMIZATION_Rabots
         public static void Main(string[] args)
         {
             Random rnd = new Random();
-            // создание 2 множеств длинной от 2 до 10 элементов
-            int[] X = new int[rnd.Next(1,10)];
-            int[] Y = new int[rnd.Next(1,10)];
-            int count = 0; //счетчик
-            Console.WriteLine("Первое множество:");
-            // создание 1 множества и его вывод
-            for (int i = 0; i < X.Length; i++)
+            int[] X = new int[rnd.Next(10)];
+            int[] Y = new int[rnd.Next(10)];
+            Console.WriteLine("Множество X: ");
+            for (var i = 0; i < X.Length; i++)
             {
-                X[i] = rnd.Next(0, 10);
+                X[i] = rnd.Next(10);
                 Console.Write(X[i] + " ");
             }
             Console.WriteLine();
-            Console.WriteLine("Второе множество:");
-            // создание и вывод 2 множества
-            for (int i = 0; i < Y.Length; i++)
+            Console.WriteLine("Множество Y: ");
+            for (var i = 0; i < Y.Length; i++)
             {
-                Y[i] = rnd.Next(0, 10);
+                Y[i] = rnd.Next(10);
                 Console.Write(Y[i] + " ");
             }
             Console.WriteLine();
-            Console.WriteLine("Кол-во общих четных элементов:");
-            // создание множества из общих элементов множеств X и Y
-            IEnumerable<int> Z = X.Intersect(Y);
-            // подсчет четных элементов в этом множестве
-            foreach (var ident in Z)
+            Console.WriteLine("Четные элементы из пересечения множеств X и Y:");
+            var Z = X.Intersect(Y);
+            int count = 0;
+            foreach (var VARIABLE in Z)
             {
-                if (ident % 2 == 0) count++;
+                if (VARIABLE % 2 == 0)
+                {
+                    count++;
+                    Console.Write(VARIABLE + " ");
+                }
             }
-            // вывод кол-ва четных элементов
-            Console.WriteLine(count);
+            if (count == 0) Console.WriteLine("Таких элементов нет");
+            
         }
     }
 }
+        
