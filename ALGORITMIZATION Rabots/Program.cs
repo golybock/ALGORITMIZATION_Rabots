a291c32b4a336b1ef0800226eac64c56e23713e3
golybock <teplovartem3094@gmail.com>
2022-01-25 16:22:58 +0300

    LR8

--- ALGORITMIZATION Rabots/Program.cs
+++ ALGORITMIZATION Rabots/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
 
 namespace ALGORITMIZATION_Rabots
 {
@@ -9,37 +9,101 @@ namespace ALGORITMIZATION_Rabots
     {
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int[] X = new int[rnd.Next(10)];
-            int[] Y = new int[rnd.Next(10)];
-            Console.WriteLine("Множество X: ");
-            for (var i = 0; i < X.Length; i++)
+            // списки на стипендии
+            List<string> commonStipendy = new List<string>();
+            List<string> niceStipendy = new List<string>();
+            Console.WriteLine("Чтобы прекратить ввод студентов, введите пустую строку в роли имени.");
+            // ввод студентов
+            for(;;)
             {
-                X[i] = rnd.Next(10);
-                Console.Write(X[i] + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Множество Y: ");
-            for (var i = 0; i < Y.Length; i++)
-            {
-                Y[i] = rnd.Next(10);
-                Console.Write(Y[i] + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Четные элементы из пересечения множеств X и Y:");
-            var Z = X.Intersect(Y);
-            int count = 0;
-            foreach (var VARIABLE in Z)
-            {
-                if (VARIABLE % 2 == 0)
+                // ввод информации о студенте
+                Console.WriteLine("Введите имя студента");
+                string name = Console.ReadLine();
+                if (name == string.Empty) break;
+                Console.WriteLine("Введите оценки ученика по одной за строку, чтобы завершить ввод, введите пустую строку:");
+                List<int> grades = new List<int>();
+                for(;;)
                 {
-                    count++;
-                    Console.Write(VARIABLE + " ");
+                    string grade = Console.ReadLine();
+                    if (grade == string.Empty) break;
+                    if (grade != null) grades.Add(Int32.Parse(grade));
+                }
+                // добавление в списки стипендий в зависимости от оценок
+                if (CheckStependy(grades) == 1) commonStipendy.Add(name);
+                if (CheckStependy(grades) == 2) niceStipendy.Add(name);
+                // запись студента в файл
+                WriteInfo(name, StringGrades(grades), AverageGrade(grades), @"D:\dan.txt");
+                Console.WriteLine("Данные записаны в файл");
+            }
+            // запись списков стипендии
+            FileStream fileStream;
+            fileStream = File.Open(@"D:\dan.txt", FileMode.Append); 
+            StreamWriter output = new StreamWriter(fileStream);
+            if (commonStipendy.Count != 0)
+            {
+                output.WriteLine("Обычная стипендия:");
+                foreach (var name in commonStipendy)
+                {
+                    output.WriteLine(name);
                 }
             }
-            if (count == 0) Console.WriteLine("Таких элементов нет");
-            
+
+            if (niceStipendy.Count != 0)
+            {
+                output.WriteLine("Повышенная стипендия:");
+                foreach (var name in niceStipendy)
+                {
+                    output.WriteLine(name);
+                }
+            }
+            output.Close();
+            // завершение
+            Console.WriteLine("Итоговый файл находится на диске D, под именем dan.txt");
         }
+        
+        // складывает все оценки и делит на их кол-во
+        private static double AverageGrade(List<int> list)
+        {
+            double value = 0;
+            foreach (var grade in list)
+            {
+                value += grade;
+            }
+            return value / list.Count;
+        }
+        // перевод список оценок в стрку, где все оценки через пробел
+        public static string StringGrades(List<int> list)
+        {
+            string strGrades = String.Empty;
+            foreach (var grade in list)
+            {
+                strGrades += " " + grade;
+            }
+            return strGrades;
+        }
+        // записывает всю информацию о студенте в файл
+        public static void WriteInfo(string name, string grades, double averageGrade, string filePath)
+        {
+            FileStream fileStream;
+            // проверяем существует ли файл файл
+            if (!File.Exists(filePath))
+                // создаем файл
+                fileStream = File.Create(filePath);
+            else
+                fileStream = File.Open(filePath, FileMode.Append); 
+            StreamWriter output = new StreamWriter(fileStream);
+            // записываем данные
+            output.WriteLine($"{name}: {grades}, Средний балл: {averageGrade}");
+            // закрываем поток
+            output.Close();
+        }
+        public static int CheckStependy(List<int> grades)
+        {
+            if (grades.Contains(3)) return 0;  // если в оценках есть 3
+            if (grades.Contains(4)) return 1;  // если в оценках нет 3 и есть 4
+            return 2;  // все оценки 5
+        }
+        
     }
 }
         
